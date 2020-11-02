3f8da385e19d55f5056b08cc552d88a307d448fb
elisabethdeborah <elisabeth_deborah@hotmail.com>
2020-11-02 15:16:28 +0100

    fullösning

--- script.js
+++ script.js
@@ -39,32 +39,29 @@ let startaNyOmgång = () => {
 // VARJE BOKSTAV I LISTAN JÄMFÖRS MED DEN VALDA BOKSTAVEN, OM DE MATCHAR LÄGGS BOKSTAVEN TILL I RÄTTBOKSTÄVER-LISTAN
 //OM DE INTE MATCHAR LÄGGS BOKSTAVEN I FELBOKSTÄVER-LISTAN
 let valdBokstav = (event) => {
-    let bokstav = event.target.innerText.toLowerCase()
+    let bokstav = event.target.innerText.toLowerCase();
     let bokstavsmätare=0;
-    console.log('Vald bokstav är: ', bokstav)
-    
+    console.log('Vald bokstav är: ', bokstav);
      ordetsBokstäver.forEach((ordetsBokstav) => {
         if(ordetsBokstav==bokstav) {
             console.log('ja')
             rättBokstäver.push(bokstav)
         } 
-         else {
-            console.log('nej')
-            felBokstäver.push(bokstav)
-        } 
-        
-    }) 
-    displayRättBokstäver();
-    displayFelBokstäver();
+    });
+//KOLLAR OM DEN VALDA BOKSTAVEN FINNS I RÄTTBOKSTÄVER-LISTAN, OM DEN FINNS BLIR BOKSTAVSMÄTAREN 1, ANNARS ÄR DEN 0
+    rättBokstäver.forEach((rättBokstav)=>{
+        if(rättBokstav==bokstav){
+            bokstavsmätare++;
+        }
+    });
+//OM BOKSTAVSMÄTAREN ÄR 0 PUSHAS VALDA BOKSTAVEN TILL FELBOKSTÄVER-LISTAN
+    if(bokstavsmätare==0) {
+        felBokstäver.push(bokstav)
+    }
+    console.log(`Rätt bokstäver: ${rättBokstäver}`)
+    console.log(`Fel bokstäver: ${felBokstäver}`)
 }
 
-let displayRättBokstäver=() => {
-    console.log(rättBokstäver)
-}
-let displayFelBokstäver=() => {
-    felBokstäver.sort()
-    console.log(felBokstäver)
-}
 
 //STARTAR NY SPELOMGÅNG GENOM ATT SLUMPA FRAM ETT ORD UR ORD-LISTAN
 startaNyOmgångKnapp.addEventListener('click', startaNyOmgång);
@@ -74,3 +71,8 @@ startaNyOmgångKnapp.addEventListener('click', startaNyOmgång);
 alfabete.forEach((bokstav) => {
     bokstav.addEventListener('click', valdBokstav);
 })
+
+
+
+
+
